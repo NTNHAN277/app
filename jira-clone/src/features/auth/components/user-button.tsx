03cc1b499f1a6e803e3cfa0a83fae4a47168f896
Nhannn277 <nhannt2.21it@vku.udn.vn>
2025-05-04 11:04:37 +0700

    slack 0.1

---
+++ jira-clone/src/features/auth/components/user-button.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useAuthActions } from "@convex-dev/auth/react";
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage
+} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../api/use-current-user";
+import { Loader, LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export const UserButton = () => {
+    const { signOut } = useAuthActions();
+    const { data, isLoading } = useCurrentUser();
+    const router = useRouter();
+
+    if (isLoading) {
+        return <Loader className="size-4 animate-spin text-muted-foreground" />
+    }
+
+    if (!data) {
+        return null;
+    }
+
+    const { image, name } = data;
+
+    const avatarFallback = name!.charAt(0).toUpperCase();
+
+    const handleSignOut = async () => {
+        await signOut();
+        router.push("/auth");
+    };
+
+    return (
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger className="outline-none relative">
+                <Avatar className="size-10 hover:opacity-75 transition">
+                    <AvatarImage alt={name} src={image} />
+                    <AvatarFallback className="bg-sky-500 text-white">
+                        {avatarFallback}
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center" side="right" className="w-60">
+                <DropdownMenuItem onClick={handleSignOut} className="h-10">
+                    <LogOut className="size-4 mr-2" />
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
